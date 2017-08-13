52eccbd19afe372fc32dcfcde200f2611b400ce2
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-08-12 23:22:50 -0700

    added more stats to table.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var paginate = require('express-paginate');
 
 Player = require('./models/player');
 
